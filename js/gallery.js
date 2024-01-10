ff8db75e895d5d4b91e8c81ad3bd9bdb7bf850da
user.email <adrian.zavoianu.n@gmail.com>
2024-01-10 23:51:52 +1300

    images update

--- js/gallery.js
+++ js/gallery.js
@@ -1,16 +1,26 @@
 /*Name this external file gallery.js*/
 
-function upDate(previewPic){
+ function upDate(previewPic){
    
        x=document.getElementById('image');
        x.innerHTML=previewPic.alt;
        link='url('+previewPic.src+')';
        x.style.backgroundImage=link;
 
-       }
+        }
    
-       function unDo(){
-        x=document.getElementById('image');
-        x.style.backgroundImage='url()';
-        x.innerHTML='Hover over an image below to display here.';
+function unDo(){
+       x=document.getElementById('image');
+       x.style.backgroundImage='url()';
+       x.innerHTML='Hover over an image below to display here.';
        }
+
+function addTabIndex(){
+       console.log("Load Test")
+       images=document.getElementsByClassName('preview')
+       for(var i=0;i<images.length;i++){
+              images[i].setAttribute('tabindex', -1)
+
+       }
+              
+}
